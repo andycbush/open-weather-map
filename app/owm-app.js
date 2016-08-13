a2fc53e1dcf76767b14c07b4757779cb1a86cc78
Andy Bush <andycbush@yahoo.com>
2016-08-13 16:27:47 -0400

    Saving animation
    This version added animation

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,7 +1,12 @@
-angular.module('OWMApp', ['ngRoute'])
+angular.module('OWMApp', ['ngRoute', 'ngAnimate'])
 
-.value('owmCities', ['New York', 'Dallas', 'Chicago'])
-    .config(function ($routeProvider) {
+.value('owmCities', [
+    'New York',
+    'Dallas',
+    'Chicago'
+])
+
+.config(function ($routeProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'home.html',
@@ -28,14 +33,22 @@ angular.module('OWMApp', ['ngRoute'])
                 redirectTo: '/error'
             });
     })
-    .run(function ($rootScope, $location) {
+    .run(function ($rootScope, $location, $timeout) {
         $rootScope.$on('$routeChangeError', function () {
-            $location.path('/error');
-        })
+            $location.path("/error");
+        });
+        $rootScope.$on('$routeChangeStart', function () {
+            $rootScope.isLoading = true;
+        });
+        $rootScope.$on('$routeChangeSuccess', function () {
+            $timeout(function () {
+                $rootScope.isLoading = false;
+            }, 1000);
+        });
     })
 
 
-.controller('HomeCtrl', function ($scope) {
+.controller('HomeCtrl', function () {
     this.welcomeMessage = 'Welcome back home!!';
 })
 
